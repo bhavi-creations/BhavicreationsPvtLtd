7e7667b32f79974b0c0172f04a4ee193aff19a27
manimalladi <manimalladi05@gmail.com>
2025-11-18 14:36:46 +0530

    add the new employee image

--- about.php
+++ about.php
@@ -1708,14 +1708,14 @@
                                         </div>
                                     </div> -->
                                     
-                                    <!-- <div class="carousel-item">
+                                    <div class="carousel-item">
                                         <div class="col-md-4">
                                             <div class="card card-body">
                                                 <img class="img-fluid"
                                                     src="assests/images/Bhavi_Creations/sathish_designer.png">
                                             </div>
                                         </div>
-                                    </div> -->
+                                    </div>
                                     <!-- <div class="carousel-item">
                                         <div class="col-md-4">
                                             <div class="card card-body">
@@ -1941,7 +1941,7 @@
                                     </div>
                                 </div>
                             </div> -->
-                            <!-- <div class="flip-card mt-4">
+                            <div class="flip-card mt-4">
                                 <div class="flip-card-inner">
                                     <div class="flip-card-front">
                                         <img src="assests/images/Bhavi_Creations/sathish_designer.png"
@@ -1952,7 +1952,7 @@
                                         <p>Designer</p>
                                     </div>
                                 </div>
-                            </div> -->
+                            </div>
 
 
 
