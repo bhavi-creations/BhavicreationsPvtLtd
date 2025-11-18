4b5688063c5749374b1e6c2abd437fd93e72a276
manimalladi <manimalladi05@gmail.com>
2025-11-18 12:47:53 +0530

    add the new employee image

--- about.php
+++ about.php
@@ -1716,14 +1716,14 @@
                                             </div>
                                         </div>
                                     </div> -->
-                                    <!-- <div class="carousel-item">
+                                    <div class="carousel-item">
                                         <div class="col-md-4">
                                             <div class="card card-body">
                                                 <img class="img-fluid"
-                                                    src="assests/images/Bhavi_Creations/abhi_ram_designer.png">
+                                                    src="assests/images/Bhavi_Creations/sai_kiran_1.png">
                                             </div>
                                         </div>
-                                    </div> -->
+                                    </div>
                                      
                                      
                                     <div class="carousel-item">
@@ -1928,19 +1928,19 @@
                                     </div>
                                 </div>
                             </div>  -->
-                            <!-- <div class="flip-card  mt-4">
+                            <div class="flip-card  mt-4">
                                 <div class="flip-card-inner">
                                     <div class="flip-card-front">
-                                        <img src="assests/images/Bhavi_Creations/abhi_ram_designer.png"
+                                        <img src="assests/images/Bhavi_Creations/sai_kiran_1.png"
                                             alt="Front Image" class="card-image img-fluid">
 
                                     </div>
                                     <div class="flip-card-back">
-                                        <p class="title">Abhi Ram</p>
+                                        <p class="title">Sai Kiran</p>
                                         <p>Designer</p>
                                     </div>
                                 </div>
-                            </div> -->
+                            </div>
                             <!-- <div class="flip-card mt-4">
                                 <div class="flip-card-inner">
                                     <div class="flip-card-front">
