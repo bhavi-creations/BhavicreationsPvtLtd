6f81f408a12139efb42099ebf8ce9041a7effde0
manimalladi <manimalladi05@gmail.com>
2025-12-23 11:09:40 +0530

    remove the empployee imagees

--- about.php
+++ about.php
@@ -1699,14 +1699,14 @@
                                         </div>
                                     </div> -->
 
-                                    <div class="carousel-item">
+                                    <!-- <div class="carousel-item">
                                         <div class="col-md-4">
                                             <div class="card card-body">
                                                 <img class="img-fluid"
                                                     src="assests/images/Bhavi_Creations/mahendra.png">
                                             </div>
                                         </div>
-                                    </div>
+                                    </div> -->
                                     
                                     <!-- <div class="carousel-item">
                                         <div class="col-md-4">
@@ -1928,7 +1928,7 @@
                                     </div>
                                 </div>
                             </div>  -->
-                            <div class="flip-card  mt-4">
+                            <!-- <div class="flip-card  mt-4">
                                 <div class="flip-card-inner">
                                     <div class="flip-card-front">
                                         <img src="assests/images/Bhavi_Creations/mahendra.png"
@@ -1940,7 +1940,7 @@
                                         <p>Designer</p>
                                     </div>
                                 </div>
-                            </div>
+                            </div> -->
                             <div class="flip-card mt-4">
                                 <div class="flip-card-inner">
                                     <div class="flip-card-front">
