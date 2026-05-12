11d913179da41f2300d8f71444967fe260fbfd49
manimalladi <manimalladi05@gmail.com>
2026-05-12 17:46:51 +0530

    seo person photo add in about page

--- about.php
+++ about.php
@@ -1916,14 +1916,14 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <!-- <div class="carousel-item">
+                                    <div class="carousel-item">
                                         <div class="col-md-4">
                                             <div class="card card-body">
                                                 <img class="img-fluid"
-                                                    src="assests/images/Bhavi_Creations/seo_social_abhi.png">
+                                                    src="assests/images/Bhavi_Creations/pavan_kumar_seo.png">
                                             </div>
                                         </div>
-                                    </div> -->
+                                    </div>
                                     <div class="carousel-item">
                                         <div class="col-md-4">
                                             <div class="card card-body">
@@ -2271,18 +2271,18 @@
                     aria-labelledby="pills-SEOTeam-kakinada-tab" tabindex="0">
                     <div class="container text-center my-3">
                         <div class="row mx-auto my-auto" style="justify-content: space-evenly;">
-                            <!-- <div class="flip-card mt-4">
+                            <div class="flip-card mt-4">
                                 <div class="flip-card-inner">
                                     <div class="flip-card-front">
-                                        <img src="assests/images/Bhavi_Creations/seo_social_abhi.png"
+                                        <img src="assests/images/Bhavi_Creations/pavan_kumar_seo.png"
                                             alt="Front Image" class="card-image img-fluid">
                                     </div>
                                     <div class="flip-card-back">
-                                        <p class="title">Dasari Kalyan Teja Abhishek</p>
+                                        <p class="title">Pavan Kumar</p>
                                         <p>SEO</p>
                                     </div>
                                 </div>
-                            </div> -->
+                            </div>
                             <div class="flip-card mt-4">
                                 <div class="flip-card-inner">
                                     <div class="flip-card-front">
